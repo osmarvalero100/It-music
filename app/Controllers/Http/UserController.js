1deebdc2938d936915545b539b059c0c505daae7
Osmar valero <osmar.valero@farmalisto.com.co>
2019-10-14 21:09:45 -0500

    Proyecto base con AdonisJs

---
+++ app/Controllers/Http/UserController.js
@@ -0,0 +1,15 @@
+'use strict'
+const Mail = use('Mail')
+class UserController {
+  async store({request}){
+    await Mail.raw('plain text email', (message) => {
+      message
+        .to('osmarvalero100@gmail.com')
+        .from('it.colombia.@gmail.com')
+        .subject('Welcome to IT MUSIC')
+    })
+    return { greeting: 'Hello world in MAIL' }
+  }
+}
+
+module.exports = UserController
