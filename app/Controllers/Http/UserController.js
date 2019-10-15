bb2cac082ed54016f632643207253a999a792b7b
Osmar valero <osmar.valero@farmalisto.com.co>
2019-10-14 22:02:29 -0500

    Utiliza plantilla html para correo

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -2,11 +2,20 @@
 const Mail = use('Mail')
 class UserController {
   async store({request}){
+    /*
     await Mail.raw('plain text email', (message) => {
       message
         .to('osmarvalero100@gmail.com')
         .from('it.colombia.@gmail.com')
         .subject('Welcome to IT MUSIC')
+    })
+    */
+
+    await Mail.send('emails.welcome', {user: "Osmar"}, (message) => {
+      message
+      .to('osmarvalero100@gmail.com')
+      .from('it.colombia.@gmail.com')
+      .subject('Welcome to IT MUSIC')
     })
     return { greeting: 'Hello world in MAIL' }
   }
