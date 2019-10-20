953fe9b4222e0872cdc1bc017d5363c352b7c6b3
Osmar valero <osmar.valero@farmalisto.com.co>
2019-10-19 21:29:04 -0500

    Migración para Grupos

--- database/migrations/1503250034280_token.js
+++ database/migrations/1503250034280_token.js
@@ -6,7 +6,7 @@ const Schema = use('Schema')
 class TokensSchema extends Schema {
   up () {
     this.create('tokens', (table) => {
-      table.increments()
+      table.uuid('id').unique().defaultTo(this.db.raw('public.gen_random_uuid()'))
       table.uuid('user_id').references('id').inTable('users')
       table.string('token', 255).notNullable().unique().index()
       table.string('type', 80).notNullable()
---
+++ database/migrations/1571537919811_group_schema.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class GroupSchema extends Schema {
+  up () {
+    this.create('groups', (table) => {
+      table.uuid('id').unique().defaultTo(this.db.raw('public.gen_random_uuid()'))
+      table.string('name', 100).notNullable()
+      table.string('description', 150)
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('groups')
+  }
+}
+
+module.exports = GroupSchema
